986633f52ff1ac8a22b6a42fb4b49fb660a4d952
Arnauld Loyer <arnauld.loyer@gmail.com>
2013-07-18 19:04:08 +0200

    Add money and price support

--- features/step_definitions/steps.clj
+++ features/step_definitions/steps.clj
@@ -1,11 +1,19 @@
 (use 'clojure.test)
 (require '[coffee-machine.core :as core])
+(import java.math.BigDecimal)
+
+
+(def TWO (BigDecimal. "2"))
 
 (def *output* (atom ""))
+(def *money* (atom TWO))
+
+(Given #"^I've inserted (\d+(?:\.\d+)?)€ in the machine$" [amount]
+      (reset! *money* (BigDecimal. amount)))
 
 (When #"^I order a '([^\']*)' with (\d+) sugar$" [drink nb-sugar-cubes]
       (let [nb-sugar (Integer/parseInt nb-sugar-cubes)
-            order (core/create-order drink nb-sugar)
+            order (core/create-order drink nb-sugar @*money*)
             output (core/process order)]
           (reset! *output* output)))
 
@@ -16,6 +24,3 @@
 (Then #"^the instruction generated should be '([^']*)'$" [expected-output]
   (is (= expected-output @*output*)))
 
-(Given #"^I've inserted (\d+(?:\.\d+)?)€ in the machine$" [arg1]
-  (comment  Express the Regexp above with the code you wish you had  )
-  (throw (cucumber.runtime.PendingException.)))
--- src/coffee_machine/core.clj
+++ src/coffee_machine/core.clj
@@ -1,27 +1,41 @@
 (ns coffee-machine.core)
+(import java.math.BigDecimal)
 
-(defrecord Drink [label protocol-part])
-(defrecord Order [drink nb-sugar])
+(defrecord Drink [label protocol-part price])
+(defrecord Order [drink nb-sugar money])
 (defprotocol Processor (process [processable]))
 
-(def all-drinks [(->Drink "Tea" "T")
-                 (->Drink "Coffee" "C")
-                 (->Drink "Chocolate" "H")])
+(def all-drinks [(->Drink "Tea" "T" (BigDecimal. "0.4"))
+                 (->Drink "Coffee" "C" (BigDecimal. "0.5"))
+                 (->Drink "Chocolate" "H" (BigDecimal. "0.6"))])
 
 (defn drink-by-label [label]
   (some #(if (.equalsIgnoreCase (:label %) label) %) all-drinks))
 
-(defn create-order [drink-label nb-sugar] 
+(defn create-order [drink-label nb-sugar money] 
   (let [drink (drink-by-label drink-label)]
-    (->Order drink nb-sugar)))
+    (->Order drink nb-sugar money)))
+
+(def ZERO BigDecimal/ZERO)
+
+(defn- missing-money [order drink]
+  (let [money (:money order)
+        drink (:drink order)
+        price (:price drink)]
+      (.subtract price money)))
 
 (extend-protocol Processor
   Order
   (process [order]
-     (let [drink-part (:protocol-part (:drink order))
-           nb-sugar   (:nb-sugar order)
-           sugar-part (if (< 0 nb-sugar) (str nb-sugar ":0") ":")]
-         (str drink-part ":" sugar-part)))
+     (let [drink (:drink order)
+           missing (missing-money order drink)]
+        (if (< 0 (.compareTo missing ZERO))
+            (process (str "Not enough money " missing " missing"))
+            ; else
+            (let [drink-part (:protocol-part drink)
+                  nb-sugar   (:nb-sugar order)
+                  sugar-part (if (< 0 nb-sugar) (str nb-sugar ":0") ":")]
+                (str drink-part ":" sugar-part)))))
   String
   (process [message]
      (str "M:" message)))
