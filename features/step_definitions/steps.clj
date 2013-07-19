0ab7c6b62e9f8608f1a924843c998d3df314a015
Arnauld Loyer <arnauld.loyer@gmail.com>
2013-07-19 18:43:20 +0200

    Add step to create array of order

--- features/step_definitions/steps.clj
+++ features/step_definitions/steps.clj
@@ -15,10 +15,13 @@
 (Given #"^I've inserted (\d+(?:\.\d+)?)€ in the machine$" [amount]
       (reset! actual-money (BigDecimal. amount)))
 
-(defn- process-order [drink nb-sugar very-hot]
-      (let [order (core/create-order drink nb-sugar @actual-money very-hot)
+(defn- process-order 
+  ([drink nb-sugar very-hot]
+      (process-order drink nb-sugar very-hot @actual-money))
+  ([drink nb-sugar very-hot money]
+      (let [order (core/create-order drink nb-sugar money very-hot)
             output (core/process order)]
-          (reset! actual-output output)))
+          (reset! actual-output output))))
 
 (When #"^I order an? '([^\']*)' with (\d+) sugar$" [drink nb-sugar-cubes]
       (process-order drink (Integer/parseInt nb-sugar-cubes) false))
@@ -35,4 +38,26 @@
 
 (Then #"^the instruction generated should be '([^']*)'$" [expected-output]
   (is (= expected-output @actual-output)))
+
+;;
+;;
+
+(Given #"^the following orders:$" [orders]
+  (comment  Express the Regexp above with the code you wish you had  )
+  (def df (java.text.SimpleDateFormat. "HH:mm:ss"))
+  (doseq [order (.asMaps orders)]
+    (let [raw-ts (.get order "time")
+          ts (.parse df raw-ts) ; unused
+          raw-drink (.get order "drink")
+          drink (core/drink-by-label raw-drink)
+          money (:price drink)]
+        (process-order raw-drink 0 false money))))
+
+(When #"^I query for a report$" []
+  (comment  Express the Regexp above with the code you wish you had  )
+  (throw (cucumber.runtime.PendingException.)))
+
+(Then #"^the report output should be$" [report-string]
+  (comment  Express the Regexp above with the code you wish you had  )
+  (throw (cucumber.runtime.PendingException.)))
 
