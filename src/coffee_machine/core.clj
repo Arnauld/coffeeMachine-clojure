2c7c9e5b41378ce99c603e20f40445dc9c02babe
Arnauld Loyer <arnauld.loyer@gmail.com>
2013-07-19 09:41:48 +0200

    Add very hot support: create-order function handle a variable number of parameter + add function to adjust nb sugar and hot behavior; refactor both for consistancy

--- src/coffee_machine/core.clj
+++ src/coffee_machine/core.clj
@@ -1,14 +1,41 @@
 (ns coffee-machine.core)
 (import java.math.BigDecimal)
 
-(defrecord Drink [label protocol-part price])
-(defrecord Order [drink nb-sugar money])
+(defrecord Drink [label protocol-part price sugar-adapter hot-adapter])
+(defrecord Order [drink nb-sugar money very-hot])
 (defprotocol Processor (process [processable]))
 
-(def all-drinks [(->Drink "Tea" "T" (BigDecimal. "0.4"))
-                 (->Drink "Coffee" "C" (BigDecimal. "0.5"))
-                 (->Drink "Chocolate" "H" (BigDecimal. "0.6"))
-                 (->Drink "Orange Juice" "O" (BigDecimal. "0.6"))])
+(defn accept-sugar [sugar] sugar)
+(defn no-sugar     [sugar] 0)
+(defn not-hot      [hot] false)
+(defn accept-hot   [hot] hot)
+
+(def all-drinks [(->Drink "Tea" "T" (BigDecimal. "0.4") accept-sugar accept-hot)
+                 (->Drink "Coffee" "C" (BigDecimal. "0.5") accept-sugar accept-hot)
+                 (->Drink "Chocolate" "H" (BigDecimal. "0.6") accept-sugar accept-hot)
+                 (->Drink "Orange Juice" "O" (BigDecimal. "0.6") no-sugar not-hot)])
+
+(defn- adapted-sugar [order]
+  (let [sugar (:nb-sugar order)
+        drink (:drink order)
+        adapter (:sugar-adapter drink)]
+    (adapter sugar)))
+
+(defn sugar-protocol-part [order]
+  (let [nb-sugar   (adapted-sugar order)]
+    (if (< 0 nb-sugar) (str nb-sugar ":0") ":")))
+
+(defn- adapted-hot [order]
+  (let [very-hot (:very-hot order)
+        drink (:drink order)
+        adapter (:hot-adapter drink)]
+    (adapter very-hot)))
+
+(defn drink-protocol-part [order]
+  (let [hot   (adapted-hot order)
+        drink (:drink order)]
+    (str (:protocol-part drink) (if hot "h" ""))))
+
 
 (defn drink-by-label [label]
   (let [found (some #(if (.equalsIgnoreCase (:label %) label) %) all-drinks)]
@@ -17,31 +44,33 @@
         ; else
         found)))
 
-(defn create-order [drink-label nb-sugar money] 
-  (let [drink (drink-by-label drink-label)]
-    (->Order drink nb-sugar money)))
+(defn create-order 
+  ([drink-label nb-sugar money] 
+    (create-order drink-label nb-sugar money false))
+  ([drink-label nb-sugar money very-hot] 
+    (let [drink (drink-by-label drink-label)]
+      (->Order drink nb-sugar money very-hot))))
 
 (def ZERO BigDecimal/ZERO)
 
-(defn- missing-money [order drink]
+(defn- missing-money [order]
   (let [money (:money order)
         drink (:drink order)
         price (:price drink)]
       (.subtract price money)))
 
+
 (extend-protocol Processor
   Order
   (process [order]
-     (let [drink (:drink order)
-           missing (missing-money order drink)]
+     (let [missing (missing-money order)]
         (if (< 0 (.compareTo missing ZERO))
             (do 
               ;(println "missing money (" (:money order) " vs " (:price (:drink order)) ")")
               (process (str "Not enough money " missing " missing")))
             ; else
-            (let [drink-part (:protocol-part drink)
-                  nb-sugar   (:nb-sugar order)
-                  sugar-part (if (< 0 nb-sugar) (str nb-sugar ":0") ":")]
+            (let [drink-part (drink-protocol-part order)
+                  sugar-part (sugar-protocol-part order)]
                 (str drink-part ":" sugar-part)))))
   String
   (process [message]
